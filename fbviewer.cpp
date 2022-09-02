e27ad00d8aac533b8d82dca2476007ba1e8029f1
James Hubbard <hubbar31@purdue.edu>
2022-09-01 21:30:15 -0400

    working on depth image in framebuffer

---
+++ fbviewer.cpp
@@ -0,0 +1,80 @@
+//fbviewer.cpp
+
+#include <linux/fb.h>
+#include "fbviewer.h"
+
+
+#define DEFAULT_DISPLAY_MODE	DISPLAY_MODE_DEPTH
+
+fbviewer* fbviewer::ms_self = NULL;
+
+fbviewer::fbviewer(const char* strSampleName, openni::Device& device, openni::VideoStream& depth, openni::VideoStream& color) :
+	m_device(device), m_depthStream(depth), m_colorStream(color), m_streams(NULL), m_eViewState(DEFAULT_DISPLAY_MODE), m_pTexMap(NULL)
+{
+	ms_self = this;
+	strncpy(m_strSampleName, strSampleName, ONI_MAX_STR - 1);
+}
+
+fbviewer::~fbviewer()
+{
+	delete[] m_pTexMap;
+
+	ms_self = NULL;
+
+	if (m_streams != NULL)
+	{
+		delete []m_streams;
+	}
+}
+
+openni::Status fbviewer::init(int argc, char **argv)
+{
+	openni::VideoMode depthVideoMode;
+	openni::VideoMode colorVideoMode;
+
+	if (m_depthStream.isValid() && m_colorStream.isValid())
+	{
+		depthVideoMode = m_depthStream.getVideoMode();
+		colorVideoMode = m_colorStream.getVideoMode();
+
+		int depthWidth = depthVideoMode.getResolutionX();
+		int depthHeight = depthVideoMode.getResolutionY();
+		int colorWidth = colorVideoMode.getResolutionX();
+		int colorHeight = colorVideoMode.getResolutionY();
+
+		m_width = depthWidth;
+		m_height = depthHeight;
+	}
+	else if (m_depthStream.isValid())
+	{
+		depthVideoMode = m_depthStream.getVideoMode();
+		m_width = depthVideoMode.getResolutionX();
+		m_height = depthVideoMode.getResolutionY();
+	}
+	else if (m_colorStream.isValid())
+	{
+		colorVideoMode = m_colorStream.getVideoMode();
+		m_width = colorVideoMode.getResolutionX();
+		m_height = colorVideoMode.getResolutionY();
+	}
+	else
+	{
+		printf("Error - expects at least one of the streams to be valid...\n");
+		return openni::STATUS_ERROR;
+	}
+
+	m_streams = new openni::VideoStream*[2];
+	m_streams[0] = &m_depthStream;
+	m_streams[1] = &m_colorStream;
+
+
+	
+	return openni::STATUS_OK;
+}
+
+openni::Status fbviewer::draw()
+{
+	
+	
+	return openni::STATUS_OK;
+}
