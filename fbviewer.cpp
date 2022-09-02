d8b43bbab1bd8846c95e8c8c74370d0668d40552
jlukehubbard <58089207+jlukehubbard@users.noreply.github.com>
2022-09-01 21:34:14 -0400

    high-tech clipboard
    copying text from one file to another through several network connections instead of one read and write

--- fbviewer.cpp
+++ fbviewer.cpp
@@ -67,7 +67,53 @@ openni::Status fbviewer::init(int argc, char **argv)
 	m_streams[0] = &m_depthStream;
 	m_streams[1] = &m_colorStream;
 
+	/* RAW INITIALIZATION AND CLEANUP CODE FROM fbtest.c !!!
+	int fbfd = 0; //framebuffer file descriptor
+	struct fb_var_screeninfo var_info_orig;
+	long int screensize = 0;
 
+	//Open the fb device file for r/w
+	fbfd = open("/dev/fb0", O_RDWR);
+	if (!fbfd) {
+		printf("Can't open framebuffer device.\n");
+		return EXIT_FAILURE;
+	}
+	printf("The framebuffer device is open.\n");
+
+	//Get variable screen info
+	if (ioctl(fbfd, FBIOGET_VSCREENINFO, &var_info)) {
+		printf("Can't read variable screen information.\n");
+	}
+	printf("Display info: %dx%d pix., %d bits/pixel\n", var_info.xres, var_info.yres, var_info.bits_per_pixel);
+
+	//store original screeninfo for later
+	memcpy(&var_info_orig, &var_info, sizeof(struct fb_var_screeninfo));
+
+	//This is where you would change the variable screen info if you wanted!!
+	var_info.bits_per_pixel = 8;
+	if(ioctl(fbfd, FBIOPUT_VSCREENINFO, &var_info)) {
+		printf("Error setting variable screeninfo.\n");
+	}
+
+	//Get fixed screen info
+	if (ioctl(fbfd, FBIOGET_FSCREENINFO, &fix_info)) {
+		printf("Can't read fixed screen information.\n");
+	}
+
+	//map fb to user memory
+	screensize = fix_info.smem_len;
+	fbp = (char*) mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+	if ((int) fbp == -1) printf("Failed to map framebuffer to user memory.\n");
+	else {
+		draw();
+		sleep(5);
+	}
+
+	//cleanup
+	munmap(fbp, screensize);
+	if(ioctl(fbfd, FBIOPUT_VSCREENINFO, &var_info_orig)) printf("Error re-setting variable screeninfo.\n");
+	close(fbfd);
+	*/
 	
 	return openni::STATUS_OK;
 }
