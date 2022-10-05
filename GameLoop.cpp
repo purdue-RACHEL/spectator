3484db5694a648c79dbeb24372910fb07793a169
Micah <micah.morefield@gmail.com>
2022-10-05 16:54:38 -0400

    Split game and camera loop

--- GameLoop.cpp
+++ GameLoop.cpp
@@ -1,70 +1,22 @@
 #include <chrono>
 #include "GameLoop.hpp"
+#include <string>
 #include <thread>
 #include "UartDecoder.hpp"
-#include <string>
 
 using clock_type = std::chrono::high_resolution_clock;
 
-int main(int argc, char ** argv){
-
-	// TODO:
-    // 1. any initial setup
-    // 2. calibrate camera?
-    // 3. calibrate projector? might need to be done manually.
-    // 4. start threads
-
-    /*
-    Thread List:
-
-    1. MainThread - poll MCU every X ms and interpret the packet.
-                    cause any changes necessary. updated variables
-                    that are needed for other threads will be read
-                    automatically in-loop - no need to alert threads.
-                    I recommend we set X to be at least 30 FPS.
-    
-    2. ProjectorThread - read any variables that are needed for display
-                         and use them to rewrite to the projector every
-                         Y ms. I recommend we set Y to be 30 FPS.
-
-    3. CameraThread - interpret camera data - this is by far the most
-                      complex thread - we may need more than 1. data
-                      from this thread does not yet have a laid out
-                      representation.
-    */
-    // I think multiple threads could be a bit overkill here. Specifically because all
-    // these events, (conduct game logic, display to projector, read from camera, etc.)
-    // should happen in the same sequence each loop of the game, and we would probably
-    // just end up locking every thread while one is running. Also managing shared 
-    // variables between threads can be a headache. I commented it out for now
-    // just to make testing simpler.
-    /*
-    std::thread mainThread(mainLoop);
-    std::thread projectorThread(projectorLoop);
-    std::thread cameraThread(cameraLoop);
-
-    // TODO: idk what to do here yet
-    mainThread.join();
-    projectorThread.join();
-    cameraThread.join();
-    */
-	mainLoop();			
-    return -1;
-}
-
-int mainLoop()
+int gameLoop()
 {
-    // v maybe accessible to projector thread
     Bounce bounce = NOBOUNCE;
     Bounce previous_bounce = NOBOUNCE;
 
-    // TODO: v must be accessible to projector thread
+    Button button = NOPRESS;
+
     uint8_t score_red = 0;
     uint8_t score_blue = 0;
 
-    // TODO: location variables
-
-    Button button = NOPRESS;
+    // TODO: location variables - multithreaded
 
     std::string deviceStr = "/dev/ttyUSB0";
 	UartDecoder uart = UartDecoder(deviceStr);
@@ -121,17 +73,5 @@ int mainLoop()
         // else - error handling
     }
 
-    return -1;
-}
-
-// TODO: move to projector file
-int projectorLoop()
-{
-    return -1;
-}
-
-// TODO: move to camera file
-int cameraLoop()
-{
     return -1;
 }
--- GameLoop.hpp
+++ GameLoop.hpp
@@ -1,7 +1,6 @@
 #ifndef INCLUDE_GAME_LOOP
 #define INCLUDE_GAME_LOOP
-int mainLoop();
-int projectorLoop();
-int cameraLoop();
+
+int gameLoop();
 
 #endif
